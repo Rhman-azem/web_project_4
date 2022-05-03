6d71fed555a5817f3921b47b81ad2b1014ce43df
Abedalrhman Azem <abedalrhman.azem@gmail.com>
2022-05-03 13:18:46 +0300

    project 4

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 ### Overview
 
-* Figma
-* Images
+- description
+- technologies I used
+- Link
 
-**Figma**
+**description**
 
-* [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+This is a project that showcases the a photo gallery which you can add photos to using the
+"+" button. it also can change the name and info of the user using an "edit" button.
 
-**Images**
+**technologies I used **
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
+The main focus was making the website responsive & displayed correctly on popular screen sizes which was possible by using **@media** & **grid**.
+I used **:hover** & **opacity** to make a the buttons change.
+Used javascript **quesrySelector** to grab the users input and displasy changes accordingly.
 
-Have fun with JavaScript!
+The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster.
+
+**Link**
+
+https://rhman-azem.github.io/web_project_4/
--- styles/javascript.js
+++ styles/javascript.js
@@ -1,38 +1,3 @@
-let yosemiteButton = document.querySelector("#yosemite");
-let lakeButton = document.querySelector("#lake");
-let mountainsButton = document.querySelector("#mountains");
-let latemarButton = document.querySelector("#latemar");
-let vanoiseButton = document.querySelector("#vanoise");
-let lagoButton = document.querySelector("#lago");
-
-yosemiteButton.addEventListener("click", LIKEyosemite);
-lakeButton.addEventListener("click", LIKElake);
-mountainsButton.addEventListener("click", LIKEmountains);
-latemarButton.addEventListener("click", LIKElatemar);
-vanoiseButton.addEventListener("click", LIKEvanoise);
-lagoButton.addEventListener("click", LIKElago);
-
-function LIKEyosemite() {
-  yosemiteButton.classList.toggle("cards__like-button_active");
-}
-function LIKElake() {
-  lakeButton.classList.toggle("cards__like-button_active");
-}
-function LIKEmountains() {
-  mountainsButton.classList.toggle("cards__like-button_active");
-}
-function LIKElatemar() {
-  latemarButton.classList.toggle("cards__like-button_active");
-}
-
-function LIKEvanoise() {
-  vanoiseButton.classList.toggle("cards__like-button_active");
-}
-
-function LIKElago() {
-  lagoButton.classList.toggle("cards__like-button_active");
-}
-/*-------------------------------------------------------------------------------------*/
 document.getElementById("edit").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "flex";
 });
@@ -46,8 +11,16 @@ function handleProfileFormSubmit(evt) {
   let showabout = document.getElementById("about");
   showname.textContent = nameInput;
   showabout.textContent = jobInput;
-  console.log(showname);
+
   document.querySelector(".popup").style.display = "none";
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+let xButton = document.querySelector(".popup__x-button");
+
+function handlePopupClose() {
+  document.querySelector(".popup").style.display = "none";
+}
+
+xButton.addEventListener("click", handlePopupClose);
